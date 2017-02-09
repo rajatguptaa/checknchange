65c776baad0c549edc97860ced08ccfe07e0f2d5
dharmendra <deoradharmendra@gmail.com>
2017-02-09 22:37:24 +0530

    Delete Old Code

--- application/libraries/my_phpimap.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class My_PhpImap {
-    public function My_PhpImap() {
-   require_once('PhpImap/__autoload.php');
-    }
-}
--- application/libraries/my_phpmailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class My_PHPMailer {
-    public function My_PHPMailer() {
-
-        require_once('PHPMailer/PHPMailerAutoload.php');
-    }
-}
